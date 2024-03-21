1be82ef4199eb2d85d49b30f4338b2df30f53e7b
shurik204 <iwmdiif@gmail.com>
2024-03-21 18:43:50 +0200

    Don't use local capture in MinecraftServerMixin

--- src/main/java/me/shurik/simplechunkmanager/mixin/MinecraftServerMixin.java
+++ src/main/java/me/shurik/simplechunkmanager/mixin/MinecraftServerMixin.java
@@ -1,28 +1,29 @@
 package me.shurik.simplechunkmanager.mixin;
 
 import me.shurik.simplechunkmanager.api.SimpleChunkManager;
-import net.minecraft.core.BlockPos;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.server.MinecraftServer;
-import net.minecraft.server.level.ServerChunkCache;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.progress.ChunkProgressListener;
-import net.minecraft.world.level.ForcedChunksSavedData;
+import net.minecraft.world.level.Level;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
-import java.util.Iterator;
+import java.util.Map;
 
-/**
- * Store the server instance in FluxNetworks.
- */
 @Mixin(value = MinecraftServer.class, priority = Integer.MIN_VALUE)
 public class MinecraftServerMixin {
-    @Inject(method = "prepareLevels", at = @At(value = "INVOKE", target = "Lit/unimi/dsi/fastutil/longs/LongSet;iterator()Lit/unimi/dsi/fastutil/longs/LongIterator;", shift = At.Shift.BEFORE, remap = false), locals = LocalCapture.CAPTURE_FAILHARD)
-    void loadChunkLoaderChunks(ChunkProgressListener progressListener, CallbackInfo info, ServerLevel overworld, BlockPos spawnPos, ServerChunkCache overworldChunkCache, Iterator<ServerLevel> levelsIterator, ServerLevel currentLevel, ForcedChunksSavedData currentLevelForcedChunksData) {
-        SimpleChunkManager.VALIDATION.invoker().validate(currentLevel, SimpleChunkManager.of(currentLevel));
-        SimpleChunkManager.of(currentLevel).submitAllTickets();
+    @Shadow @Final private Map<ResourceKey<Level>, ServerLevel> levels;
+
+    @Inject(method = "prepareLevels", at = @At(value = "RETURN"))
+    void loadChunkLoaderChunks(ChunkProgressListener progressListener, CallbackInfo info) {
+        for (ServerLevel serverLevel : this.levels.values()) {
+            SimpleChunkManager.VALIDATION.invoker().validate(serverLevel, SimpleChunkManager.of(serverLevel));
+            SimpleChunkManager.of(serverLevel).submitAllTickets();
+        }
     }
 }
